ed22a9a212d95c3dc0227908453887559905a36e
palmaguer <pablodanielalmaguer@gmail.com>
2024-09-30 17:00:21 -0500

    feat: improve OAuth2 class for flexibility and maintainability.
     - Refactor OAuth2 class to dynamically load environment configuration via the Config class.
     - Add support for passing envfile to Config constructor for flexible environment handling.
     - Move token-related properties (accessToken, expiresIn, tokenType) outside the constructor.
     - Implement getter and setter methods for class properties for better encapsulation.
     - Improve logging with detailed debug messages for easier troubleshooting.
     - Refactor token endpoint construction to use dynamic API endpoint from Config.
     - Enhance error handling with detailed logging of errors during token retrieval.
     - Make `getToken` method more flexible by allowing optional overriding of parameters.

--- src/api/oauth2.js
+++ src/api/oauth2.js
@@ -1,30 +1,67 @@
 // src/api/auth.js
-const { identityConfig } = require('../config/config');
+const Config = require('../config/config');
 const logger = require('../utils/logger'); // Import the logger
 const axios = require('axios');
 
 
-let accessToken = null;
-
 class OAuth2 {
-  constructor() {
-    this.apiUrl = `${identityConfig.apiBaseUrl}${identityConfig.tokenEndpoint}`; // Construct the token URL
-    this.accessToken = null;
-    this.expiresIn = null;
-    this.tokenType = null;
+  // OAuth2 Token properties
+  accessToken = null;
+  expiresIn = null;
+  tokenType = null;
 
-    logger.debug(`Token URL initialized: ${this.apiUrl}`); // Log the initialized token URL
+  constructor(envfile = null) {
+    this.config = new Config(envfile); // Pass the envfile to Config
+
+    if (envfile != null) {
+      const oauthConfig = this.config.getIdentityOAuthConfig(); // Get the identity configuration
+
+      // Environment validation moved to config.js
+      this.apiBaseUrl = oauthConfig.apiBaseUrl; // Construct the token URL
+      this.clientId = oauthConfig.clientId;
+      this.clientSecret = oauthConfig.clientSecret;
+      this.clientScope = oauthConfig.clientScope;
+
+      logger.debug('OAuth2: environment "%s" loaded. %o', envfile, oauthConfig);
+    }
   }
 
-  async getToken(clientId = identityConfig.clientId, clientSecret = identityConfig.clientSecret, clientScope = identityConfig.clientScope) {
+  // Getter and Setter for envfile
+  getEnvFile() { return this.envfile; }
+  setEnvFile(envfile) { 
+    logger.debug(`OAuth2: setting environment file "${envfile}"`);
+    this.envfile = envfile; 
+  }
+  // Getter and Setter for accessToken
+  getAccessToken() { return this.accessToken; }
+  setAccessToken(token) { this.accessToken = token; }
+  // Getter and Setter for expiresIn
+  getExpiresIn() { return this.expiresIn; }
+  setExpiresIn(expirationTime) { this.expiresIn = expirationTime; }
+  // Getter and Setter for tokenType
+  getTokenType() { return this.tokenType; }
+  setTokenType(type) { this.tokenType = type; }
+  
+  // Check if the token is still valid
+  // isTokenValid() {
+  //   return this.accessToken && this.expiresIn && (Date.now() < (this.expiresIn * 1000));
+  // }
+
+  async getToken(identityUrl = this.apiBaseUrl,
+                 clientId = this.clientId, 
+                 clientSecret = this.clientSecret,
+                 clientScope = this.clientScope) {
     try {
+      const apiEndpoint = Config.identityApi.tokenEndpoint;
+      const tokenApiUrl = identityUrl + apiEndpoint;
+
       // Create URLSearchParams for form-urlencoded data
       const urlencoded = new URLSearchParams();
       urlencoded.append('grant_type', 'client_credentials');
-      urlencoded.append('scope', identityConfig.clientScope);
+      urlencoded.append('scope', clientScope);
 
-      logger.debug('Requesting OAuth2 token with clientId: %s', clientId);
-      const response = await axios.post(this.apiUrl, urlencoded.toString(),
+      logger.debug('OAuth2: [%s] requesting token using clientId: %s', tokenApiUrl, clientId);
+      const response = await axios.post(tokenApiUrl, urlencoded.toString(),
         {
           auth: {
             username: clientId,
@@ -35,10 +72,20 @@ class OAuth2 {
           }
         }
       );
-      logger.debug('Token obtained successfully: %s', JSON.stringify(response.data)); // Log the successful token retrieval
-      this.accessToken = response.data.access_token;
-      this.tokenType = response.data.token_type;
-      this.expiresIn = response.data.expires_in;
+      
+      this.setAccessToken(response.data.access_token);
+      this.setTokenType(response.data.token_type);
+      this.setExpiresIn(response.data.expires_in);
+      
+      // Log the successful token retrieval
+      logger.debug('Token obtained successfully: %o',
+        JSON.stringify(
+          {
+            "accessToken": `${this.getAccessToken().substring(0, 20)}...`,
+            "tokenType": this.getTokenType(),
+            "expiresIn": this.getExpiresIn()
+          }
+        ), null, 5); 
 
       return response.data; // Return the token data
     } catch (error) {
@@ -46,9 +93,5 @@ class OAuth2 {
       throw error; // Re-throw the error for further handling if needed
     }
   }
-  // Method to get the stored access token
-  //getAccessToken() {
-  //  return this.accessToken; // Return the internal access token
-  //}
 }
-module.exports = new OAuth2();
+module.exports = OAuth2;
