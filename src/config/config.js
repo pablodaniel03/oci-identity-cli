fcff3428b55f672ceace26fb13f6a4ae7734b134
palmaguer <pablodanielalmaguer@gmail.com>
2024-09-30 16:48:16 -0500

    feat: convert config to class-base module.
     - API Endpoints are now a static class-property
     - Can dinamically load environment files via constructor parameter resolving its filepath.
     - Getter/Setter methods for class properties.
     - Full encapsulation of both data and behaivor.
     - More flexibility to extend functionality and configuration.

--- src/config/config.js
+++ src/config/config.js
@@ -3,39 +3,54 @@ const path = require('path');
 const dotenv = require('dotenv');
 const logger = require('../utils/logger');
 
-// Function to load the environment variables based on the environment argument
-function loadEnv(env) {
-    const envFilePath = path.resolve(__dirname, './env', `${env}.env`); // Adjusted path to point to /env
+
+class Config {
+  // List of supported APIs
+  static identityApi = {
+                  tokenEndpoint: '/oauth2/v1/token',
+                   userEndpoint: '/admin/v1/Users',
+                  groupEndpoint: '/admin/v1/Groups',
+      userStatusChangerEndpoint: '/admin/v1/UserStatusChanger',
+    userPasswordChangerEndpoint: '/admin/v1/UserPasswordChanger',
+           jobSchedulesEndpoint: '/job/v1/JobSchedules'
+  };
+
+  constructor(envfile) {
+    if (envfile != null) {
+      // Load environment variables from the envfile provided via command-line argument.
+      this.loadEnvironment(envfile);
+
+      // Set configuration properties
+      this.identityOAuthConfig = {
+          apiBaseUrl: process.env.OCI_IDENTITY_URL,
+            clientId: process.env.OCI_OAUTH_CLIENT,
+        clientSecret: process.env.OCI_OAUTH_SECRET,
+         clientScope: process.env.OCI_OAUTH_SCOPE
+      };
+      logger.debug('Identity Config: %s', JSON.stringify(this.identityConfig));
+    }
+  }
+
+  // Method to load environment variables
+  loadEnvironment(envfile) {
+    // Check if the provided envfile is an absolute path or relative
+    const envFilePath = path.isAbsolute(envfile)
+                        ? envfile
+                        : path.resolve(process.cwd(), envfile); // Use current working directory
     const envFile = dotenv.config({ path: envFilePath });
 
     if (envFile.error) {
-      logger.error(`Error loading environment variables from ${envFilePath}:`, envFile.error);
+      logger.error(`Error loading environment variables from file "${envFilePath}": "${envFile.error}"`);
       throw envFile.error; // Optionally throw an error to stop execution
     }
-    
-    logger.debug(`Loaded environment variables from ${envFilePath}`);
+
+    logger.debug(`Loaded environment variables from file "${envFilePath}"`);
+  }
+
+  // Method to get the identity configuration
+  getIdentityOAuthConfig() {
+    return this.identityOAuthConfig;
+  }
 }
 
-// Read the environment argument passed to the script
-const environment = process.env.ENVIRONMENT || 'development'; // process.argv[2] || Default to 'development'
-loadEnv(environment);
-
-// Configuration object
-const identityConfig = {
-    apiBaseUrl: process.env.OCI_IDENTITY_URL,
-    clientId: process.env.OCI_OAUTH_CLIENT,
-    clientSecret: process.env.OCI_OAUTH_SECRET,
-    clientScope: process.env.OCI_OAUTH_SCOPE,
-    tokenEndpoint: '/oauth2/v1/token',
-    userEndpoint: '/admin/v1/Users',
-    groupEndpoint: '/admin/v1/Groups',
-    userStatusChangerEndpoint: '/admin/v1/UserStatusChanger',
-    userPasswordChangerEndpoint: '/admin/v1/UserPasswordChanger',
-    jobSchedulesEndpoint: '/job/v1/JobSchedules',
-};
-
-logger.debug('Identity Config: %s', JSON.stringify(identityConfig));
-
-module.exports = {
-  identityConfig
-};
+module.exports = Config; // Now export the Config class instead of an instance. So we can provide the envfile.
