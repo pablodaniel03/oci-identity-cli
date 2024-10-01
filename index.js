702e358efb5bb3a6191b62cb73d5ebe4afcc0f2b
palmaguer <pablodanielalmaguer@gmail.com>
2024-09-30 22:11:25 -0500

    style: removing commented out code

--- index.js
+++ index.js
@@ -8,27 +8,6 @@ program
     .description('CLI for interacting with Oracle Identity Cloud Service')
     .version('1.0.0', '-v, --version', 'Output the current program version');
 
-// Add a general help command
-// program
-//     .command('help')
-//     .description('Display help for available commands')
-//     .action(() => {
-//         console.log(`
-// Available Commands:
-//   auth [command]         Commands related to authentication
-//   userSearch [command]   Commands related to user management
-
-// Commands for 'auth':
-//   obtain-token           Obtain an access token
-
-// Commands for 'userSearch':
-//   list                  List all users
-//   get <userId>         Get user by ID
-
-// Use 'node src/index.js [command] --help' for more information about a specific command.
-//         `);
-//     });
-
 
 program.parse(process.argv);
 
