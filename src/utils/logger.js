9f0d60d9096ecc12c9bcd086e2dbe2468bbbe44b
palmaguer <pablodanielalmaguer@gmail.com>
2024-10-02 00:55:17 -0500

    chore: change default log level to 'fatal'

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,7 +3,7 @@ const pino = require('pino');
 
 // Initialize Pino logger
 const logger = pino({
-    level: process.env.LOG_LEVEL || 'info', // Set log level from env or default to 'info'
+    level: process.env.LOG_LEVEL || 'fatal', // Set log level from env or default to 'info'
     transport: {
         target: 'pino-pretty', // Pretty print for console output
         options: {
