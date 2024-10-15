246eb3343e6f38ba9040ceb272603e3973da3900
palmaguer <pablodanielalmaguer@gmail.com>
2024-10-15 15:48:17 -0500

    chore: ehnance handleQuoting function to catch errors when replacing string

--- src/utils/handleCommandArgs.js
+++ src/utils/handleCommandArgs.js
@@ -1,17 +1,29 @@
 const os = require('os'); // Import os module for platform detection
+const logger = require('./logger');
+
 
 // Function to format the filter based on the OS
 function handleQuoting(argument) {
   const platform = os.platform();
+  
+  logger.debug({argument}, `hangleQuoting: starting function (os=${platform})`);
 
-  handledArg = argument.replace(/'/g, '\"');
+  try {
+    handledArg = argument.replace(/'/g, '\"');
 
-  if (platform === 'win32') {
-    // For Windows CMD, escape quotes
-    return handledArg.replace(/"/g, '\"');
-  } else {
-    // For Linux/macOS, normal quoting
-    return handledArg;
+    if (platform === 'win32') {
+      return handledArg.replace(/"/g, '\"'); // For Windows CMD, escape quotes
+    } else {
+      return handledArg; // For Linux/macOS, normal quoting
+    }
+  } catch (error) {
+    const message = {
+      code: "HANDLE_QUOTE_ERR",
+      detail: error.message
+    };
+
+    logger.error(message, 'handleQuoting: error while replacing quotes.');
+    throw message;
   }
 }
 
